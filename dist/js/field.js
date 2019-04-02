0682abd36a244adf46fa7ca88f08cbd6c21a8344
Rehan Aslam <advoor@gmail.com>
2019-04-02 21:15:57 +0100

    Handle loading and saving of data

--- dist/js/field.js
+++ dist/js/field.js
@@ -438,6 +438,9 @@ var LinkTool = __webpack_require__(18);
          */
         setInitialValue: function setInitialValue() {
 
+            var self = this;
+            var currentContent = JSON.parse(self.field.value);
+
             var editor = new EditorJS({
                 /**
                  * Wrapper of Editor
@@ -447,9 +450,6 @@ var LinkTool = __webpack_require__(18);
                  * Tools list
                  */
                 tools: {
-                    /**
-                     * Each Tool is a Plugin. Pass them via 'class' option with necessary settings {@link docs/tools.md}
-                     */
                     header: {
                         class: Header,
                         inlineToolbar: ['link'],
@@ -458,9 +458,6 @@ var LinkTool = __webpack_require__(18);
                         },
                         shortcut: 'CMD+SHIFT+H'
                     },
-                    /**
-                     * Or pass class directly without any configuration
-                     */
                     list: {
                         class: List,
                         inlineToolbar: true,
@@ -484,96 +481,19 @@ var LinkTool = __webpack_require__(18);
                 /**
                  * This Tool will be used as default
                  */
-                // initialBlock: 'paragraph',
+                initialBlock: 'paragraph',
+
                 /**
                  * Initial Editor data
                  */
-                data: {
-                    blocks: [{
-                        type: "header",
-                        data: {
-                            text: "Editor.js",
-                            level: 2
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.'
-                        }
-                    }, {
-                        type: "header",
-                        data: {
-                            text: "Key features",
-                            level: 3
-                        }
-                    }, {
-                        type: 'list',
-                        data: {
-                            items: ['It is a block-styled editor', 'It returns clean data output in JSON', 'Designed to be extendable and pluggable with a simple API'],
-                            style: 'unordered'
-                        }
-                    }, {
-                        type: "header",
-                        data: {
-                            text: "What does it mean «block-styled editor»",
-                            level: 3
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.'
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.'
-                        }
-                    }, {
-                        type: "header",
-                        data: {
-                            text: "What does it mean clean data output",
-                            level: 3
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below'
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.'
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'Clean data is useful to sanitize, validate and process on the backend.'
-                        }
-                    }, {
-                        type: 'paragraph',
-                        data: {
-                            text: 'We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it\'s core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏'
-                        }
-                    }, {
-                        type: 'image',
-                        data: {
-                            "file": {
-                                "url": "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg"
-                            },
-                            "caption": "Roadster // tesla.com",
-                            "withBorder": false,
-                            "withBackground": false,
-                            "stretched": true
-                        }
-                    }]
-                },
+                data: currentContent,
                 onReady: function onReady() {},
                 onChange: function onChange() {
-                    console.log('something changed');
+                    editor.save().then(function (savedData) {
+                        self.handleChange(savedData);
+                    });
                 }
             });
-
-            this.value = this.field.value || '';
         },
 
 
@@ -589,7 +509,7 @@ var LinkTool = __webpack_require__(18);
          * Update the field's internal value.
          */
         handleChange: function handleChange(value) {
-            this.value = value;
+            this.value = JSON.stringify(value);
         }
     }
 });
@@ -10976,7 +10896,7 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c(
     "default-field",
-    { attrs: { field: _vm.field, errors: _vm.errors } },
+    { attrs: { field: _vm.field, errors: _vm.errors, fullWidthContent: true } },
     [
       _c("template", { slot: "field" }, [
         _c("div", { attrs: { id: "editorjs" } })
--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,5 +1,5 @@
 <template>
-    <default-field :field="field" :errors="errors">
+    <default-field :field="field" :errors="errors" :fullWidthContent="true">
         <template slot="field">
             <div id="editorjs"></div>
         </template>
@@ -29,6 +29,9 @@ export default {
          */
         setInitialValue() {
 
+            let self = this;
+            let currentContent = JSON.parse(self.field.value);
+
             var editor = new EditorJS({
                 /**
                  * Wrapper of Editor
@@ -38,9 +41,6 @@ export default {
                  * Tools list
                  */
                 tools: {
-                    /**
-                     * Each Tool is a Plugin. Pass them via 'class' option with necessary settings {@link docs/tools.md}
-                     */
                     header: {
                         class: Header,
                         inlineToolbar: ['link'],
@@ -49,9 +49,6 @@ export default {
                         },
                         shortcut: 'CMD+SHIFT+H'
                     },
-                    /**
-                     * Or pass class directly without any configuration
-                     */
                     list: {
                         class: List,
                         inlineToolbar: true,
@@ -75,117 +72,21 @@ export default {
                 /**
                  * This Tool will be used as default
                  */
-                // initialBlock: 'paragraph',
+                initialBlock: 'paragraph',
+
                 /**
                  * Initial Editor data
                  */
-                data: {
-                    blocks: [
-                        {
-                            type: "header",
-                            data: {
-                                text: "Editor.js",
-                                level: 2
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : 'Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration.'
-                            }
-                        },
-                        {
-                            type: "header",
-                            data: {
-                                text: "Key features",
-                                level: 3
-                            }
-                        },
-                        {
-                            type : 'list',
-                            data : {
-                                items : [
-                                    'It is a block-styled editor',
-                                    'It returns clean data output in JSON',
-                                    'Designed to be extendable and pluggable with a simple API',
-                                ],
-                                style: 'unordered'
-                            }
-                        },
-                        {
-                            type: "header",
-                            data: {
-                                text: "What does it mean «block-styled editor»",
-                                level: 3
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : 'Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor\'s Core.'
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : `There are dozens of <a href="https://github.com/editor-js">ready-to-use Blocks</a> and the <a href="https://editorjs.io/creating-a-block-tool">simple API</a> for creation any Block you need. For example, you can implement Blocks for Tweets, Instagram posts, surveys and polls, CTA-buttons and even games.`
-                            }
-                        },
-                        {
-                            type: "header",
-                            data: {
-                                text: "What does it mean clean data output",
-                                level: 3
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : 'Classic WYSIWYG-editors produce raw HTML-markup with both content data and content appearance. On the contrary, Editor.js outputs JSON object with data of each Block. You can see an example below'
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : `Given data can be used as you want: render with HTML for <code class="inline-code">Web clients</code>, render natively for <code class="inline-code">mobile apps</code>, create markup for <code class="inline-code">Facebook Instant Articles</code> or <code class="inline-code">Google AMP</code>, generate an <code class="inline-code">audio version</code> and so on.`
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : 'Clean data is useful to sanitize, validate and process on the backend.'
-                            }
-                        },
-                        {
-                            type : 'paragraph',
-                            data : {
-                                text : 'We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it\'s core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏'
-                            }
-                        },
-                        {
-                            type: 'image',
-                            data: {
-                                "file": {
-                                    "url" : "https://www.tesla.com/tesla_theme/assets/img/_vehicle_redesign/roadster_and_semi/roadster/hero.jpg"
-                                },
-                                "caption" : "Roadster // tesla.com",
-                                "withBorder" : false,
-                                "withBackground" : false,
-                                "stretched" : true
-                            }
-                        },
-                    ]
-                },
+                data: currentContent,
                 onReady: function(){
 
                 },
                 onChange: function() {
-                    console.log('something changed');
+                    editor.save().then((savedData) => {
+                        self.handleChange(savedData)
+                    });
                 }
             });
-
-
-            this.value = this.field.value || ''
         },
 
         /**
@@ -199,7 +100,7 @@ export default {
          * Update the field's internal value.
          */
         handleChange(value) {
-            this.value = value
+            this.value = JSON.stringify(value)
         },
     },
 }
