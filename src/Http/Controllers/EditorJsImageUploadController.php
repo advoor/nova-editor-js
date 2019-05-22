50efc2554f512c59548d852f4ea683a5d0a591be
Rehan Aslam <advoor@gmail.com>
2019-05-22 21:57:19 +0100

    Image alterations!

--- src/Http/Controllers/EditorJsImageUploadController.php
+++ src/Http/Controllers/EditorJsImageUploadController.php
@@ -6,6 +6,8 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Spatie\Image\Exceptions\InvalidManipulation;
+use Spatie\Image\Image;
 
 class EditorJsImageUploadController extends Controller
 {
@@ -31,6 +33,10 @@ class EditorJsImageUploadController extends Controller
             config('nova-editor-js.toolSettings.image.path'),
             config('nova-editor-js.toolSettings.image.disk')
         );
+
+
+        $this->applyAlterations(Storage::disk(config('nova-editor-js.toolSettings.image.disk'))->path($path));
+       // $this->applyThumbnails(Storage::disk(config('nova-editor-js.toolSettings.image.disk'))->path($path));
 
         return [
             'success' => 1,
@@ -85,5 +91,78 @@ class EditorJsImageUploadController extends Controller
                 'url' => Storage::disk(config('nova-editor-js.toolSettings.image.disk'))->url($nameWithPath)
             ]
         ];
+    }
+
+    /**
+     * @param $path
+     */
+    private function applyAlterations($path)
+    {
+        try {
+            $image = Image::load($path);
+
+            $imageSettings = config('nova-editor-js.toolSettings.image.alterations');
+
+            if (!empty($imageSettings['resize']['width'])) {
+                $image->width($imageSettings['resize']['width']);
+            }
+
+            if (!empty($imageSettings['resize']['height'])) {
+                $image->height($imageSettings['resize']['height']);
+            }
+
+            if (!empty($imageSettings['optimize'])) {
+                $image->optimize();
+            }
+
+            if (!empty($imageSettings['optimize'])) {
+                $image->optimize();
+            }
+
+            if (!empty($imageSettings['adjustments']['brightness'])) {
+                $image->brightness($imageSettings['adjustments']['brightness']);
+            }
+
+            if (!empty($imageSettings['adjustments']['contrast'])) {
+                $image->contrast($imageSettings['adjustments']['contrast']);
+            }
+
+            if (!empty($imageSettings['adjustments']['gamma'])) {
+                $image->gamma($imageSettings['adjustments']['gamma']);
+            }
+
+            if (!empty($imageSettings['effects']['blur'])) {
+                $image->blur($imageSettings['effects']['blur']);
+            }
+
+            if (!empty($imageSettings['effects']['pixelate'])) {
+                $image->pixelate($imageSettings['effects']['pixelate']);
+            }
+
+            if (!empty($imageSettings['effects']['greyscale'])) {
+                $image->greyscale();
+            }
+            if (!empty($imageSettings['effects']['sepia'])) {
+                $image->sepia();
+            }
+
+            if (!empty($imageSettings['effects']['sharpen'])) {
+                $image->sharpen($imageSettings['effects']['sharpen']);
+            }
+
+            $image->save();
+
+        } catch (InvalidManipulation $exception) {
+            report($exception);
+        }
+    }
+
+    private function applyThumbnails($path)
+    {
+        $image = Image::load($path);
+
+        $imageSettings = config('nova-editor-js.toolSettings.image');
+
+        dd($imageSettings);
     }
 }
