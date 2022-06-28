ee6d33a410df08a87dae980a111b3706ce9e44ce
Rehan Aslam <rehan.aslam@stickee.co.uk>
2022-06-28 16:41:45 +0100

    The string is passed through?

--- src/NovaEditorJsData.php
+++ src/NovaEditorJsData.php
@@ -9,6 +9,24 @@ use Stringable;
 
 class NovaEditorJsData extends Fluent implements Htmlable, Stringable
 {
+    /**
+     *
+     * Create a new fluent instance.
+     *
+     * @param iterable<TKey, TValue> $attributes
+     * @return void
+     */
+    public function __construct($attributes = [])
+    {
+        if (is_string($attributes)) {
+            $attributes = json_decode($attributes);
+        }
+
+        foreach ($attributes as $key => $value) {
+            $this->attributes[$key] = $value;
+        }
+    }
+
     public function toHtml()
     {
         return NovaEditorJs::generateHtmlOutput($this);
@@ -16,6 +34,6 @@ class NovaEditorJsData extends Fluent implements Htmlable, Stringable
 
     public function __toString()
     {
-        return (string) $this->toHtml();
+        return (string)$this->toHtml();
     }
 }
