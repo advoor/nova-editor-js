2b2a5199e7d1085225a7cc6b42488c20ed5b2800
Roelof Roos <github@roelofr.dev>
2022-09-25 14:39:55 +0200

    Added some opinionated entries to php-cs-fixer
    Remove useless phpdoc comments, and format phpunit tests consistently

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -19,6 +19,13 @@ return (new Config())
     ->setRules([
         '@PSR12' => true,
 
+        // Consistent naming of PHPUnit methods
+        'php_unit_method_casing' => ['case' => 'camel_case'],
+        'php_unit_test_annotation' => ['style' => 'prefix'],
+
+        // Remove duplicate entries from phpdoc (like `@param string` on a typed method, without further info)
+        'no_superfluous_phpdoc_tags' => true,
+
         // Ensure strict types are used
         'declare_strict_types' => true,
         'strict_param' => true,
