e90c134e3570fd76fd89376bf3af5162e68a1134
Mior Muhammad Zaki <crynobone@gmail.com>
2022-10-25 19:47:48 +0800

    wip

--- nova.mix.js
+++ nova.mix.js
@@ -5,15 +5,6 @@ const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
 
-const novaLocation = [
-    path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js'),
-    path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-].filter(fs.existsSync)[0] ?? '';
-
-if (!novaLocation) {
-    throw new Error('Unable to locate Nova resources. Mount the extension in a Laravel installation, or run `composer install` in the extension directory.');
-}
-
 class NovaExtension {
     name() {
         return 'nova-extension';
@@ -33,11 +24,11 @@ class NovaExtension {
     webpackConfig(webpackConfig) {
         webpackConfig.externals = {
             vue: 'Vue',
+            'laravel-nova': 'LaravelNova'
         };
 
         webpackConfig.resolve.alias = {
             ...(webpackConfig.resolve.alias || {}),
-            'laravel-nova': novaLocation,
         };
 
         webpackConfig.output = {
