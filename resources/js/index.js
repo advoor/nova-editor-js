52cec26678c652032dea0a479876e4ffccfec47d
Angelos Michalopoulos <a-mi@otenet.gr>
2023-06-12 23:24:01 +0300

    change order of blocks

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,19 +5,19 @@ import NovaEditorJS from './nova-editor';
 window.NovaEditorJS = new NovaEditorJS();
 
 // Import the blocks
-require('./blocks/checklist');
-require('./blocks/code');
-require('./blocks/delimiter');
-require('./blocks/embed');
 require('./blocks/heading');
 require('./blocks/image');
+require('./blocks/list');
+require('./blocks/checklist');
+require('./blocks/table');
+require('./blocks/delimiter');
+require('./blocks/code');
+require('./blocks/raw');
+require('./blocks/embed');
 require('./blocks/inline-code');
 require('./blocks/link');
-require('./blocks/list');
 require('./blocks/marker');
 require('./blocks/paragraph');
-require('./blocks/raw');
-require('./blocks/table');
 
 // Import the Nova field declaration
 require('./field');
