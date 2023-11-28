14f6bf99a22dd33e5c1d5eeca9ad92aa4d67dd95
Woeler <woeler@woeler.eu>
2023-11-28 19:35:28 +0100

    Update EditorJsImageUploaded.php

--- src/Events/EditorJsImageUploaded.php
+++ src/Events/EditorJsImageUploaded.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Advoor\NovaEditorJs\Events;
 
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 
 class EditorJsImageUploaded
